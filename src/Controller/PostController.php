a44f0249d52ab608c99c083dedb1a4df605ec382
Emilie <emilie.chnvt@gmail.com>
2025-04-23 17:19:38 +0200

    User entity added

---
+++ src/Controller/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
+
+final class PostController extends AbstractController
+{
+    #[Route('/posts', name: 'app_posts')]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/post/show/{id}', name: 'app_posts_show', priority: -1)]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+}
